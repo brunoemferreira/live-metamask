c42874126a7a42fe0c5e0a2a7ad6f3c14e8cbb39
Bruno E. M. Ferreira <brunoemf@gmail.com>
2021-12-28 21:22:00 -0300

    Arquivos iniciais do projeto Live Metamask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState('');
+
+  async function connect() {
+
+    // Verifica se a metamask está instalado no navegador
+    if (!window.ethereum)
+      return setMessage('No MetaMask installed!');
+    // Mostra uma mensagem que esta tentando se conectar
+    setMessage('Trying to connect....');
+
+    await window.ethereum.send('eth_requestAccounts');
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+
+    // Ver saldo da carteira 
+    const balance = await provider.getBalance('passar aqui o hash da carteira');
+    setMessage(ethers.utils.formatEther(balance.toString))
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="button" value="Connect" onClick={event => connect()} />
+      {message}
     </div>
   );
 }
